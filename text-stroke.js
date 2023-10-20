61e0db80c3669e5d02b582029b04590b2986170e
HolmesBryant <148395713+HolmesBryant@users.noreply.github.com>
2023-10-20 13:24:38 -0500

    added files

--- text-stroke.js
+++ text-stroke.js
@@ -1,59 +1,118 @@
+/**
+ *  Renders a stroke around some text
+ *
+ *  @author Holmes Bryant <webbmaastaa@gmail.com>
+ *  @license GPL-3.0
+ *
+ *  @attribute [strokecolor] optional (default: "silver") The color of the stroke.
+ *  @attribute [strokewidth] optional (default: "2px") The width of the stroke.
+ *
+ *  @usage
+ *  	<script type="module" src="text-stroke.js"></script>
+ *  	<text-stroke>Some text to stroke</text-stroke>
+ */
 export class TextStroke extends HTMLElement {
-	shadow = {ShadowRoot};
-	color = 'black';
-	width = 2;
+	shadow = ShadowRoot;
+	neg = '-2px';
+	pos = '2px';
 	measure = 'px';
-	static observedAttributes = ['width', 'color'];
+	#strokecolor = 'silver';
+	#strokewidth = '2';
+	static observedAttributes = ['strokewidth', 'strokecolor'];
 
 	constructor() {
 		super();
 		this.shadow = this.attachShadow({mode: 'open'});
-		this.color = this.getAttribute('color') || this.color;
-		this.setWidth(this.getAttribute('width'));
+		this.shadow.innerHTML = `
+		<style>
+			::slotted(*) {
+		        text-shadow:
+		        ${this.neg} ${this.neg} 0 ${this.strokecolor},
+		        0 ${this.neg} 0 ${this.strokecolor},
+		        ${this.pos} ${this.neg} 0 ${this.strokecolor},
+		        ${this.pos} 0 0 ${this.strokecolor},
+		        ${this.pos} ${this.pos} 0 ${this.strokecolor},
+		        0 ${this.pos} 0 ${this.strokecolor},
+		        ${this.neg} ${this.pos} 0 ${this.strokecolor},
+		        ${this.neg} 0 0 ${this.strokecolor};
+			}
+		</style>
+		<slot></slot>
+		`;
 	}
 
 	connectedCallback() {
-		const css = this.setCss();
-		const tmpl = this.setTemplate();
-		this.shadow.append(css);
-		this.shadow.append(tmpl);
-	}
-
-	setWidth(width = {String}) {
-		if (typeof width === 'string') {
-			const arr = width.match(/(\d+)(\D*)/);
-			this.measure = arr.pop();
-			this.width = arr.pop();
+		if (this.children.length < 1) {
+			this.wrapTextInsideElement('h1');
 		}
+		this.#strokewidth = this.getAttribute('strokewidth') || this.strokewidth;
+		this.#strokecolor = this.getAttribute('strokecolor') || this.strokecolor;
+		this.update(this.#strokewidth);
 	}
 
-	setTemplate() {
-		const tmpl = `<slot></slot>`;
-		return document.createRange().createContextualFragment(tmpl);
+	attributeChangedCallback(attr, oldval, newval) {
+		this[attr] = newval;
+	}
+
+	update(width) {
+		const el = this.shadow.querySelector('style');
+
+		if (typeof width === 'string' && isNaN(width)) {
+			const arr = width.match(/([\d.]+)(\D*)/);
+			this.measure = arr.pop();
+			width = arr.pop();
+		}
+
+		this.pos = width + this.measure;
+		this.neg = (width * -1) + this.measure;
+		this.setCss();
 	}
 
 	setCss() {
-		const color = this.color;
-		const pos = this.width + this.measure;
-		const neg = (this.width * -1) + this.measure;
 		const css = `
 			<style>
 			::slotted(*) {
 		        text-shadow:
-		        ${neg} ${neg} 0 ${color},
-		        0 ${neg} 0 ${color},
-		        ${pos} ${neg} 0 ${color},
-		        ${pos} 0 0 ${color},
-		        ${pos} ${pos} 0 ${color},
-		        0 ${pos} 0 ${color},
-		        ${neg} ${pos} 0 ${color},
-		        ${neg} 0 0 ${color};
+		        ${this.neg} ${this.neg} 0 ${this.strokecolor},
+		        0 ${this.neg} 0 ${this.strokecolor},
+		        ${this.pos} ${this.neg} 0 ${this.strokecolor},
+		        ${this.pos} 0 0 ${this.strokecolor},
+		        ${this.pos} ${this.pos} 0 ${this.strokecolor},
+		        0 ${this.pos} 0 ${this.strokecolor},
+		        ${this.neg} ${this.pos} 0 ${this.strokecolor},
+		        ${this.neg} 0 0 ${this.strokecolor};
 			}
 			</style>
 		`;
 
-		return document.createRange().createContextualFragment(css);
+		const frag = document.createRange().createContextualFragment(css);
+		const el = this.shadow.querySelector('style');
+		this.shadow.replaceChild(frag, el);
 	}
+
+	wrapTextInsideElement(elemTxt) {
+		const elem = document.createElement(elemTxt);
+		elem.innerText = this.innerHTML.trim();
+		this.innerHTML = '';
+		this.append(elem);
+	}
+
+	get strokecolor() { return this.#strokecolor; }
+	set strokecolor(value) {
+		if (!value) value = 'inherit';
+		this.#strokecolor = value;
+		this.update(this.strokewidth);
+		// console.log('color', this.#strokecolor);
+	}
+
+	get strokewidth() { return this.#strokewidth; }
+	set strokewidth(value) {
+		if (!value) value = '2px';
+		this.#strokewidth = value;
+		this.update(value);
+		// console.log('width', this.#strokewidth);
+	}
+
 }
 
 document.addEventListener('DOMContentLoaded', customElements.define('text-stroke', TextStroke));
